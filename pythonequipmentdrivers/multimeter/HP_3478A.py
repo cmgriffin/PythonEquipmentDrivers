c0f3eeeedc6190fb9de7cb4f9cb8598ac7641651
cmgriffin <cmg9329@gmail.com>
2021-06-14 22:26:19 -0400

    More additions to HP3478A - range lookup table

--- pythonequipmentdrivers/multimeter/HP_3478A.py
+++ pythonequipmentdrivers/multimeter/HP_3478A.py
@@ -10,6 +10,14 @@ class HP_3478A(_Scpi_Instrument):
                             'ADC': "F5",
                             'AAC': "F6",
                             'OHMS': "F3"}
+        self.ranges = {
+            'VDC': {0.03: 'R-2', 0.3: 'R-1', 3.0: 'R0', 30.0: 'R1', 300.0: 'R2'},
+            'VAC': {0.3: 'R-1', 3.0: 'R0', 30.0: 'R1', 300.0: 'R2'},
+            'ADC': {0.3: 'R-1', 3.0: 'R0'},
+            'AAC': {0.3: 'R-1', 3.0: 'R0'},
+            'OHMS': {30: 'R1', 300: 'R1', 3E3: 'R3',
+                     3E4: 'R4', 3e5: 'R5', 3E6: 'R6', 3E7: 'R7'}
+        }
         return None
 
     def set_mode(self, mode):
@@ -29,3 +37,7 @@ class HP_3478A(_Scpi_Instrument):
         else:
             raise ValueError("Invalid mode option")
         return None
+
+    def measure(self):
+
+        return float(self.instrument.read())
