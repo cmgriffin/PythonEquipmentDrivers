b99b632da8917edf4f125feb100ffeb0a89a63f2
cgriffin <cgriffin@vicr.com>
2021-11-19 17:15:44 -0500

    Some small changes to cleanup implementation

--- pythonequipmentdrivers/scripts/ped.py
+++ pythonequipmentdrivers/scripts/ped.py
@@ -12,7 +12,6 @@ from pathlib import Path
 import tkinter
 import shutil
 from tkinter.filedialog import askopenfilename
-import json
 import argparse
 import pythonequipmentdrivers as ped
 
@@ -21,20 +20,29 @@ config_path = Path.home() / CONFIG_PATH
 
 
 def check_if_setup():
+    """
+    Parse the command line arguments and determine either to run in setup mode or normal mode
+    """
     parser = argparse.ArgumentParser(
         description='Run the pythonequiptmentdrivers CLI')
-    parser.add_argument('--setup', action='store_true')
+    parser.add_argument('--setup', action='store_true',
+                        help='run the configuration setup')
     args = parser.parse_args()
     return args.setup
 
 
 def run_cli():
+    """
+    Run the command line interface (user application) after initalizing equiptment 
+    variables to the namespace.
+    Use Ipython if it is installed else use standard python
+    """
     if not config_path.is_file():
         raise FileNotFoundError(
             f'{config_path} file not found \n run "ped --setup" to configure')
     temp = ped.EnvironmentSetup(config_path)
     vars().update(vars(temp))  # transfer
-    banner = '\n\nPythonEquiptmentDrivers CLI\n'
+    banner = '\n\nPythonEquiptmentDrivers CLI\n' + globals()['__doc__']
     try:
         import IPython
         IPython.embed(colors="neutral", banner1=banner)
@@ -47,6 +55,13 @@ def run_cli():
 
 
 def run_setup():
+    """
+    Peform the initial setup to configure the json file
+    """
+    # initialize the GUI for the file selection dialogs
+    root = tkinter.Tk()
+    root.withdraw()
+    root.wm_attributes("-topmost", 1)
     json_path = askopenfilename(
         title='Select the bench configuration json file...', filetypes=[("json", '.json')])
     if json_path == '':
@@ -59,6 +74,7 @@ def run_setup():
         # directory already exists
         pass
     shutil.copyfile(json_path, config_path)
+    print('Setup completed successfully!')
 
 
 def main():
